a71af9799d5cf82563d5e81171b067571cdfe1aa
YogandherMurali <YogandherMurali@oneadvanced.com>
2023-05-05 07:23:44 +0530

    Added Rating controller

---
+++ Controllers/RatingController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AFSAPIService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RatingController : ControllerBase
+    {
+		
+	}
+}
+
